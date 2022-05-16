8d6e5a327b5300c8933fe94eef6fda1b8614a7f3
meu nome é <henristoco@gmail.com>
2022-05-16 09:59:10 -0300

    Criando projeto

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textEstilo}>calma ai</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textEstilo: {
+    color: '#ffffff'
+  }
+});
